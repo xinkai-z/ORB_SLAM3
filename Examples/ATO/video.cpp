3971d476784a40222607a8c13097d0e1008c1417
xinkai-z <zhangxinkai1990@gmail.com>
2022-10-28 22:54:28 -0700

    pure localization from pre-built map

--- Examples/ATO/video.cpp
+++ Examples/ATO/video.cpp
@@ -11,16 +11,27 @@
 
 using namespace std;
 
+/***************** Paths for config files ******************/
+
 string parameterFile = "/home/xinkaiz/projects/ato_slam_dev/Examples/ATO/ato.yaml";
 string vocFile = "/home/xinkaiz/projects/ato_slam_dev/Vocabulary/ORBvoc.txt";
 
 // video file
-// string videoFile = "/home/xinkaiz/projects/ATO/data/circuit-20210813/mp4/2021-08-13-19-52-48.mp4";
-string videoFile = "/home/xinkaiz/projects/ATO/data/circuit-20210813/mp4/2021-08-13-21-04-13.mp4";
+// string videoFile = "/home/xinkaiz/projects/ATO/data/circuit-20210813/mp4/2021-08-13-19-52-48.mp4"; // map
+// string videoFile = "/home/xinkaiz/projects/ATO/data/circuit-20210813/mp4/2021-08-13-21-04-13.mp4";
+// string videoFile = "/home/xinkaiz/projects/ATO/data/circuit-20210813/mp4/2021-08-13-22-59-29.mp4"; // reverse
+
+string outputTraj = "CameraTrajectory.txt";
+string outputKeyFrameTraj = "KeyFrameTrajectory.txt";
+
+
+void SavePCD(std::vector<ORB_SLAM3::MapPoint*> mapStuff, string file_name);
 
 int main(int argc, char **argv) {
 
-    ORB_SLAM3::System SLAM(vocFile, parameterFile, ORB_SLAM3::System::MONOCULAR, true, 0, "circuit2");
+    ORB_SLAM3::System SLAM(vocFile, parameterFile, ORB_SLAM3::System::MONOCULAR, true, 0, outputTraj);
+
+    float imageScale = SLAM.GetImageScale();
 
     // get image from video
     cv::VideoCapture cap(videoFile);    // change to 1 if you want to use USB camera.
@@ -28,38 +39,72 @@ int main(int argc, char **argv) {
     auto fps = cap.get(cv::CAP_PROP_FPS);
     auto totalframes = cap.get(cv::CAP_PROP_FRAME_COUNT);
 
-    // std::cout << "VIDEO INFO: fps: " << fps << ", total frames: " << totalframes << std::endl;
+    std::cout << "VIDEO INFO: fps: " << fps << ", total frames: " << totalframes << std::endl;
 
     // recrd time
-    auto start = chrono::system_clock::now();
+    auto time_start = chrono::system_clock::now();
 
-    while (1) {
+    double time_fps = 0.0;
+    double time_fps_diff = double(1/fps);
+
+    for (int frame_cnt=0; frame_cnt<totalframes; frame_cnt++){
+
         cv::Mat frame;
         cap >> frame;
         if ( frame.data == nullptr ){
             std::cout << "FAILED TO LOAD VIDEO!" << std::endl;
             break;
         }
-        // auto timestamp_mp4 = cap.get((cv::CAP_PROP_POS_MSEC));
 
-        auto now = chrono::system_clock::now();
+        if(imageScale != 1.f)
+        {
+            int width = frame.cols * imageScale;
+            int height = frame.rows * imageScale;
+            cv::resize(frame, frame, cv::Size(width, height));
+        }
 
-        auto timestamp = chrono::duration_cast<chrono::milliseconds>(now - start);
-        SLAM.TrackMonocular(frame, double(timestamp.count())/1000.0); //frame_resized
-        // std:cout << "Time stamp now: mp4: " << timestamp_mp4 << ", chrono: " << double(timestamp.count())/1000.0 << std::endl;
-        cv::waitKey(10);
+        auto timestamp_mp4 = cap.get((cv::CAP_PROP_POS_MSEC));
+        auto time_now = chrono::system_clock::now();
+        auto timestamp = chrono::duration_cast<chrono::milliseconds>(time_now - time_start);
+
+        // SLAM.TrackMonocular(frame, double(timestamp.count())/1000.0); //frame_resized
+
+        SLAM.TrackMonocular(frame, time_fps); //frame_resized
+        time_fps = time_fps + time_fps_diff;
+
+        // auto now = chrono::system_clock::now();
+
+        // auto timestamp = chrono::duration_cast<chrono::milliseconds>(now - start);
+        // SLAM.TrackMonocular(frame, double(timestamp.count())/1000.0); //frame_resized
+        // std::cout << "Time stamp now: mp4: " << timestamp_mp4 << ", chrono: " << double(timestamp.count())/1000.0 << std::endl;
+        // cv::waitKey(10);
+
     }
+
 
     // cout << "ttrack_tot = " << ttrack_tot << std::endl;
     // Stop all threads
     std::vector<ORB_SLAM3::MapPoint*> mapStuff = SLAM.GetAtlas()->GetCurrentMap()->GetAllMapPoints();
-        // Map* GetCurrentMap();
-        // mapStuff = SLAM.GetTrackedMapPoints();
-        cout << "Start to write PCD with datapoints: " << endl;
-        cout << mapStuff.size() << endl;
-        // std::cout << "# x,y,z" << std::endl;
-        string pathSaveFileName = "./";
-        pathSaveFileName = pathSaveFileName.append("ATO");
+    // Map* GetCurrentMap();
+    // mapStuff = SLAM.GetTrackedMapPoints();
+    std::cout << "Start to write PCD with datapoints: " << std::endl;
+    std::cout << mapStuff.size() << std::endl;
+    // std::cout << "# x,y,z" << std::endl;
+    SavePCD(mapStuff, "ATO");
+    std::cout << "End to write PCD" << std::endl;
+    SLAM.Shutdown();
+    SLAM.SaveTrajectoryEuRoC(outputTraj);
+    SLAM.SaveKeyFrameTrajectoryEuRoC(outputKeyFrameTraj);
+    std::cout << "Yo Shutting" << std::endl;
+
+    return 0;
+}
+
+
+void SavePCD(std::vector<ORB_SLAM3::MapPoint*> mapStuff, string file_name){
+
+    string pathSaveFileName = "./";
+        pathSaveFileName = pathSaveFileName.append(file_name);
         pathSaveFileName.append(".pcd");
         std::remove(pathSaveFileName.c_str());
         std::ofstream ofs(pathSaveFileName, std::ios::binary);
@@ -84,12 +129,4 @@ int main(int argc, char **argv) {
         ofs << v.x()  << " " << v.y()  << " " << v.z()  << "\n";
 	}
     ofs.close();
-    cout << "End to write PCD" << endl;
-    SLAM.Shutdown();
-    SLAM.SaveTrajectoryEuRoC("CameraTrajectory.txt");
-    SLAM.SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
-    cout << "Yo Shutting" << endl;
-
-    // SLAM.Shutdown();
-    return 0;
 }
